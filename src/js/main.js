5815472ce95188d199e3ff95298a544e6489ddef
Yurii Adamenko <skyey07@gmail.com>
2019-07-11 15:34:27 +0300

    change functions: validate, allLetter, ValidateEmail, ValidatePhone, chooseCountry, chooseState, chooseCity

--- src/js/main.js
+++ src/js/main.js
@@ -1,121 +1,172 @@
 function validate() {
+    let uname = document.getElementById('first_name'),
+        email = document.getElementById('email'),
+        phone_number = document.getElementById('phone_number');
 
-    let fld = document.getElementById('first_name');
-    let letters = /^[A-Za-z]+$/;
-
-    let closest = fld.closest("div");
-    if (fld.value.length === 0) {
-        closest.classList.add("error");
-        alert("Name should not be empty");
-        return false;
-    } else {
-        closest.classList.remove("error");
-        closest.classList.add("success");
+    if (allLetter(uname)) {
+        if (ValidateEmail(email)) {
+            if (ValidatePhone(phone_number)) {
+                if (chooseCountry(selectedCountry)) {
+                    if (chooseState(select_states)) {
+                        if (chooseCity(select_city)) {
+                            return true
+                        }
+                    }
+                }
+            }
+        }
     }
-    if (!fld.value.match(letters)) {
-        closest.classList.add("error");
-        alert("The name should contain only letters");
-        return false;
-    } else {
-        closest.classList.remove("error");
-        closest.classList.add("success");
-    }
-
-    // ==========mail============
-    let email = document.getElementById('email');
-    let validateMail = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-
-    if (email.value.length === 0) {
-        let closest = email.closest("div");
-        closest.classList.add("error");
-        alert("Email field should not be empty");
-        return false;
-    }
-    if (!email.value.match(validateMail)) {
-        let closest = email.closest("div");
-        closest.classList.add("error");
-        alert("You have entered an invalid email address!");
-        return false;
-    }
-
-    // ==========Phone============
-    let phone_number = document.getElementById('phone_number');
-    let validatePhone = /^\d{11}$/;
-
-    if (!phone_number.value.match(validatePhone)) {
-        let closest = phone_number.closest("div");
-        closest.classList.add("error");
-        alert("You have entered an invalid Phone number");
-
-        return false
-    }
-
-    return true;
+    return false;
 }
 
+function allLetter(uname) {
+    let letters = /^[A-Za-z]+$/;
+    if (uname.value.match(letters)) {
+        let closest = uname.closest("div");
+        closest.classList.remove("error");
+        uname.classList.remove("invalid");
+        closest.classList.add("success");
+        uname.classList.add("valid");
+        return true;
+    } else {
+        let closest = uname.closest("div");
+        closest.classList.add("error");
+        uname.classList.add("invalid");
+        // alert('The name should contain only letters');
+        uname.focus();
+        return false;
+    }
+}
+
+
+function ValidateEmail(email) {
+    let mailformat = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    if (email.value.match(mailformat)) {
+        let closest = email.closest("div");
+        closest.classList.remove("error");
+        closest.classList.add("success");
+
+        email.classList.remove("invalid");
+        email.classList.add("valid");
+        return true;
+    } else {
+        let closest = email.closest("div");
+        closest.classList.add("error");
+        email.classList.add("invalid");
+        // alert("You have entered an invalid email address!");
+        email.focus();
+        return false;
+    }
+}
+
+function ValidatePhone(phone_number) {
+    let valNum = /^\d{11}$/;
+    if (phone_number.value.match(valNum)) {
+        let closest = phone_number.closest("div");
+        closest.classList.remove("error");
+        closest.classList.add("success");
+
+        phone_number.classList.remove("invalid");
+        phone_number.classList.add("valid");
+        return true;
+    } else {
+        let closest = phone_number.closest("div");
+        closest.classList.add("error");
+        phone_number.classList.add("invalid");
+        // alert("You have entered an invalid Phone number");
+        phone_number.focus();
+        return false;
+    }
+}
+
+// // ======textarea=======
 let textarea = document.getElementById("textareaID");
 textarea.addEventListener("input", function () {
     let lng = this.value.length;
     let maxlength = textarea.getAttribute("maxlength");
     console.log(this.value.length);
     document.getElementById("charcount").innerHTML = lng + ' / 500 ';
-
     if (lng >= maxlength) {
         alert('You have reached the maximum length of characters')
+
+        // let closest = textarea.closest("div");
+        // closest.classList.add("active");
+
+    }
+});
+
+let x = document.querySelector(".input-field label");
+textarea.addEventListener("focus", function() {
+    let lng = this.value.length;
+    if (lng === 0){
+        x.classList.add("active");
+    }
+});
+textarea.addEventListener("blur", function() {
+    let lng = this.value.length;
+    if (lng === 0){
+        x.classList.remove("active");
+    } else {
+        x.classList.add("active");
     }
 });
 
 
-// COUNTRY
-function region() {
-    let selectedCountry = document.getElementById("select_countries");
-    let country = document.getElementById("country");
-    let selectedCountryVal = document.getElementById("select_countries").value;
-    let closest = selectedCountry.closest("div");
+let selectedCountry = document.getElementById("select_countries");
+let select_states = document.getElementById("select_states");
+let select_city = document.getElementById("select_city");
 
+function chooseCountry(selectedCountry) {
+    // selectedCountry = document.getElementById("select_countries").value;
+    let closest = document.getElementById("select_countries").closest("div");
+    // console.log(selectedCountry);
     if (document.getElementById("select_countries").value === "Default") {
-
-        alert('Select your selectedCountry from the list');
+        // alert('Select your selectedCountry from the list');
         closest.classList.add("error");
-
         return false;
     } else {
         document.getElementById("select_states").disabled = false;
-
         closest.classList.remove("error");
+        closest.classList.add("success");
         return true;
     }
+
 }
 
-function region2() {
-    let select_states = document.getElementById("select_states");
-    let closest = select_states.closest("div");
-
+function chooseState(select_states) {
+    // select_states = document.getElementById("select_states").value;
+    // console.log(select_states);
+    // let select_states = document.getElementById("select_states");
+    let closest = document.getElementById("select_states").closest("div");
     if (document.getElementById("select_states").value === "Default") {
-        alert('Select your select_states from the list');
+        // alert('Select your select_states from the list');
         closest.classList.add("error");
         return false;
     } else {
-        closest.classList.remove("error");
         document.getElementById("select_city").disabled = false;
+        closest.classList.remove("error");
+        closest.classList.add("success");
         return true;
     }
 }
 
-function region3() {
-    let select_city = document.getElementById("select_city");
-    let closest = select_city.closest("div");
-
+function chooseCity(select_city) {
+    // select_city = document.getElementById("select_city").value;
+    // console.log(select_city);
+    let closest = document.getElementById("select_city").closest("div");
     if (document.getElementById("select_city").value === "Default") {
-        alert('Select your select_city from the list');
+        // alert('Select your select_city from the list');
         closest.classList.add("error");
         return false;
     } else {
         document.getElementById("select_city").disabled = false;
         closest.classList.remove("error");
+        closest.classList.add("success");
         return true;
     }
 }
+
+
 
 axios.get('http://127.0.0.1:3000/countries')
     .then(function (response) {
@@ -245,7 +296,7 @@ axios.get('http://127.0.0.1:3000/users')
 let form = document.getElementById('formID');
 form.addEventListener('submit', function (evt) {
     evt.preventDefault();
-    if (validate() && region() && region2() && region3()) {
+    if (validate() && chooseCountry() && chooseState() && chooseCity()) {
         console.log('post request');
 
         let first_name = document.getElementById('first_name').value,
